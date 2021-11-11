1ed2644f00716b71f1a1b24d990bbe70eaeb4fef
Aldion <aldion.sumampouw@gmail.com>
2021-11-11 17:35:31 +0800

    add submission 2 folder

---
+++ movieshowapp2/app/src/main/java/com/aldion/movieshowsapp/data/ShowEntity.kt
@@ -0,0 +1,15 @@
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ShowEntity(
+    var id:String,
+    var category:String,
+    var title: String,
+    var year: String,
+    var genre: String,
+    var actor: String,
+    var userRating: String,
+    var description: String,
+    var imagePath: Int
+) : Parcelable
---
+++ movieshowapp2/app/src/test/java/com/aldion/movieshowsapp/DetailShowViewModelTest.kt
@@ -0,0 +1,52 @@
+package com.aldion.movieshowsapp
+
+import com.aldion.movieshowsapp.ui.detail.DetailShowViewModel
+import com.aldion.movieshowsapp.utils.DataDummy
+import junit.framework.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class DetailShowViewModelTest {
+
+    private lateinit var viewModel: DetailShowViewModel
+    private val dummyFilm = DataDummy.generateDummyFilms()[0]
+    private val dummyShows = DataDummy.generateDummyTvShows()[0]
+
+    @Before
+    fun setUp() {
+        viewModel = DetailShowViewModel()
+    }
+
+    @Test
+    fun setMovieShows() {
+        viewModel.setMovieShows(dummyFilm.id, dummyFilm.category)
+        val movieShow = viewModel.movieShow
+        assertNotNull(movieShow)
+        assertEquals(dummyFilm.id, movieShow.id)
+        assertEquals(dummyFilm.category, movieShow.category)
+        assertEquals(dummyFilm.title, movieShow.title)
+        assertEquals(dummyFilm.imagePath, movieShow.imagePath)
+        assertEquals(dummyFilm.year, movieShow.year)
+        assertEquals(dummyFilm.genre, movieShow.genre)
+        assertEquals(dummyFilm.userRating, movieShow.userRating)
+        assertEquals(dummyFilm.actor, movieShow.actor)
+        assertEquals(dummyFilm.description, movieShow.description)
+    }
+
+    @Test
+    fun setTVShows() {
+        viewModel.setMovieShows(dummyShows.id, dummyShows.category)
+        val tvShow = viewModel.movieShow
+        assertNotNull(tvShow)
+        assertEquals(dummyShows.id, tvShow.id)
+        assertEquals(dummyShows.category, tvShow.category)
+        assertEquals(dummyShows.title, tvShow.title)
+        assertEquals(dummyShows.imagePath, tvShow.imagePath)
+        assertEquals(dummyShows.year, tvShow.year)
+        assertEquals(dummyShows.genre, tvShow.genre)
+        assertEquals(dummyShows.userRating, tvShow.userRating)
+        assertEquals(dummyShows.actor, tvShow.actor)
+        assertEquals(dummyShows.description, tvShow.description)
+    }
+}
