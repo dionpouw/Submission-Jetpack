43cffbe51b4eade502002c7599eb7d053119c7ab
Aldion <aldion.sumampouw@gmail.com>
2021-11-15 19:47:28 +0800

    milestone commit layout

--- moviecatalog/app/src/main/java/com/aldion/moviecatalog/SplashActivity.kt
+++ moviecatalog/app/src/main/java/com/aldion/moviecatalog/SplashActivity.kt
@@ -1,11 +1,22 @@
 package com.aldion.moviecatalog
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.aldion.moviecatalog.databinding.ActivitySplashBinding
+import com.aldion.moviecatalog.ui.home.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
+    private var binding: ActivitySplashBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        Thread.sleep(2000)
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent)
+
+        finish()
     }
 }
